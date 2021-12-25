aab8ed2cb84830610fbc25ee555d6e5bf2064b94
MartinLupa <lupa_martin@hotmail.com>
2021-12-25 20:03:41 +0100

    Local JSON fetch working, Card and Main updated

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,17 +1,13 @@
-import './ProductCard.css';
+import "./ProductCard.css";
 
 export default function ProductCard({ img, model, type, full_price }) {
   return (
     <div className="card-container">
-      <img
-        className="card-img"
-        src="https://www.cervelo.com/media/catalog/product/m/y/my22-dura-ace-di2-five-black.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=800&width=1200&canvas=1200:800"
-        alt=""
-      />
-      <h2>Cervélo S5</h2>
-      <p>Road bike - Full carbon</p>
+      <img className="card-img" src={img} alt={model} />
+      <h2>{model}</h2>
+      <p>{type}</p>
       <div className="card-price">
-        <p>€2350</p>
+        <p>€{full_price}</p>
       </div>
       <button className="btn card-btn">ADD TO CART</button>
     </div>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,33 @@
-import ProductCard from '../components/ProductCard/ProductCard';
-import './Main.css';
+import { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard/ProductCard";
+import "./Main.css";
 
 export default function Main() {
+  const [catalog, setCatalog] = useState([]);
+  useEffect(() => {
+    fetch("catalog.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setCatalog(data.bicycles));
+  }, []);
+
   return (
     <div className="main-container">
-      <ProductCard />
+      {catalog.map((product, index) => {
+        return (
+          <ProductCard
+            key={product.product_id}
+            img={product.image}
+            model={product.model}
+            type={product.type}
+            full_price={product.full_price}
+          />
+        );
+      })}
     </div>
   );
 }
