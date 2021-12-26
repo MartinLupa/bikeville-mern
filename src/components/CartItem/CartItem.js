3b78fe35ec4d5161470847bb03617c3a8049c1db
MartinLupa <lupa_martin@hotmail.com>
2021-12-26 13:04:16 +0100

    CartItem and ShopingCart update, improved responsiveness, CartItemButton

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,17 +1,31 @@
+import CartItemButton from "../CartItemButton/CartItemButton";
 import "./CartItem.css";
 
 export default function CartItem() {
   return (
     <div className="cart-item">
-      <div className="qty-container">
-        <div className="qty">1</div>
+      <div className="cart-left">
+        <div className="qty-container">
+          <div className="qty">1</div>
+        </div>
+        <img
+          className="cart-img"
+          src="https://www.cervelo.com/media/catalog/product/m/y/my22-dura-ace-di2-five-black.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=800&width=1200&canvas=1200:800"
+          alt=""
+        />
+        <div>
+          <h5>Cervelo R5</h5>
+          <p>ID: RB_R501</p>
+        </div>
       </div>
-      <img
-        className="cart-img"
-        src="https://www.cervelo.com/media/catalog/product/m/y/my22-dura-ace-di2-five-black.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=800&width=1200&canvas=1200:800"
-        alt=""
-      />
-      <h6>Cervelo R5</h6>
+      <div className="cart-right">
+        <div className="cart-price">€2450</div>
+        <div>
+          <CartItemButton text={"+"} />
+          <CartItemButton text={"-"} />
+          <CartItemButton text={"x"} />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/CartItemButton/CartItemButton.js
@@ -0,0 +1,5 @@
+import "./CartItemButton.css";
+
+export default function CartItemButton({ text }) {
+  return <button className="cart-item-btn">{text}</button>;
+}
