c256962b84e10f70dd61ac3955bc1a4d04af9191
MartinLupa <lupa_martin@hotmail.com>
2021-12-27 16:18:44 +0100

    Navbar links update, MaterialUI Badge for shoppingCart

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import DirectionsBikeIcon from "@mui/icons-material/DirectionsBike";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import Badge from "@mui/material/Badge";
 import { NavLink } from "react-router-dom";
 import "../../styles/Variables.scss";
 import "./Navbar.css";
@@ -7,16 +8,35 @@ import "./Navbar.css";
 export default function Navbar() {
   return (
     <div className="navbar-container">
-      <div className="nav-left">
-        <DirectionsBikeIcon fontSize="large" />
-        <h4>BIKEVILLE</h4>
-      </div>
+      <NavLink to="/">
+        <div className="nav-left">
+          <DirectionsBikeIcon className="nav-link" fontSize="large" />
+          <h4 className="nav-link">BIKEVILLE</h4>
+        </div>
+      </NavLink>
+
       <div className="nav-center">
-        <input className="input" type="text" />
+        <input
+          className="input"
+          type="text"
+          placeholder="Search by product name"
+        />
       </div>
       <div className="nav-right">
+        <NavLink className="nav-link" to="/registration">
+          <p>REGISTER</p>
+        </NavLink>
+        <NavLink className="nav-link" to="/login">
+          <p>LOGIN</p>
+        </NavLink>
         <NavLink to="/shopping_cart/martin">
-          <ShoppingCartOutlinedIcon fontSize="medium" className="cart-icon" />
+          <Badge badgeContent={2} color="primary">
+            <ShoppingCartOutlinedIcon
+              fontSize="large"
+              className="nav-link"
+              color="action"
+            />
+          </Badge>
         </NavLink>
       </div>
     </div>
