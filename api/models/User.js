ab08c4542695b13f5725175abeabe71ee2089d04
MartinLupa <lupa_martin@hotmail.com>
2021-12-29 17:55:37 +0100

    REST API, mongodb, stripe, full CRUD, admin and customer roles

---
+++ api/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const { boolean } = require("webidl-conversions");
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
---
+++ client/src/pages/Pay/Pay.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import StripeCheckout from "react-stripe-checkout";
+import GeneralButton from "../../components/GeneralButton/GeneralButton";
+import "./Pay.css";
+
+export default function Pay() {
+  const [stripeToken, setStripeToken] = useState(null);
+  const navigate = useNavigate();
+
+  const onToken = (token) => {
+    setStripeToken(token);
+  };
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await axios.post(
+          "http://localhost:5000/api/checkout/payment",
+          { tokenId: stripeToken.id, amount: 2000 }
+        );
+        console.log(res.data);
+        navigate("/success");
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    stripeToken && makeRequest();
+  }, [stripeToken]);
+
+  return (
+    <div className="pay-container">
+      {stripeToken ? (
+        <span>Processing. Please wait...</span>
+      ) : (
+        <StripeCheckout
+          name="Bike Ville"
+          billingAddress
+          shippingAddress
+          description="Your total is 20"
+          amount={2000}
+          token={onToken}
+          stripeKey="pk_test_51KC2PyFVmRibgb7cIfnBbyEhMhTQDOus9i0naKot6ArtGLdNLbI4WuJqtzbF4o4z87PiMjRBzQ9QTOuhwXmMUvRE00ZihdQeV2"
+        >
+          <GeneralButton text={"PAY"} />
+        </StripeCheckout>
+      )}
+    </div>
+  );
+}
