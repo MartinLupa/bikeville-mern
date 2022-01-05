dfa3206ee44dc9571b3d81b491fbc2f156487ea3
MartinLupa <lupa_martin@hotmail.com>
2022-01-05 07:47:31 +0100

    Formik and Yup validation added to Login

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,23 +1,55 @@
+import { Form, Formik } from "formik";
 import { Link } from "react-router-dom";
+import * as Yup from "yup";
+import { FormField } from "../FormField/FormField";
 import GeneralButton from "../GeneralButton/GeneralButton";
 import "./Login.css";
 
+const initialValues = {
+  email: "",
+  password: "",
+};
+
+const onSubmit = (values) => {
+  console.log(values);
+};
+
+const validationSchema = Yup.object({
+  email: Yup.string().email("Invalid email format").required("Required"),
+  password: Yup.string().required("Required"),
+});
+
 export default function Login() {
   return (
-    <form className="login-form">
-      <div className="form-col">
-        <label htmlFor="">Email</label>
-        <input type="text" placeholder="Enter your email" />
-        <label htmlFor="">Password</label>
-        <input type="password" placeholder="Enter your password" />
-      </div>
-      <div className="login-links">
-        <Link to="/registration">Create an account</Link>
-        <Link to="/password_recovery">Forgot your password?</Link>
-      </div>
-      <div className="form-btn">
-        <GeneralButton text={"LOGIN"} />
-      </div>
-    </form>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
+    >
+      <Form className="login-form" action="" method="POST">
+        <FormField
+          fieldName={"email"}
+          labelName={"Email"}
+          name={"email"}
+          placeholder={"Enter a registered email"}
+          type={"email"}
+        />
+        <FormField
+          fieldName={"password"}
+          labelName={"Password"}
+          name={"password"}
+          placeholder={"Enter your password"}
+          type={"password"}
+        />
+
+        <div className="login-links">
+          <Link to="/registration">Create an account</Link>
+          <Link to="/password_recovery">Forgot your password?</Link>
+        </div>
+        <div className="form-btn">
+          <GeneralButton type={"submit"} text={"LOGIN"} />
+        </div>
+      </Form>
+    </Formik>
   );
 }
