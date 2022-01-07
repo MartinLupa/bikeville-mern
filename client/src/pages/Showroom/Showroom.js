1604fec2446f5d420b9c86e55add0a48302557d7
MartinLupa <lupa_martin@hotmail.com>
2022-01-07 15:03:56 +0100

    fetch request to products db successfull

--- client/src/pages/Showroom/Showroom.js
+++ client/src/pages/Showroom/Showroom.js
@@ -16,14 +16,25 @@ export default function Showroom() {
     productsToRender = catalog;
   }
   useEffect(() => {
-    fetch("catalog.json", {
+    // fetch("catalog.json", {
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Accept: "application/json",
+    //   },
+    // })
+
+    fetch("http://localhost:5000/api/products", {
+      method: "GET",
+
       headers: {
         "Content-Type": "application/json",
-        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZDgyNzk2YTNlNzY5NzcwOTE0ZjVhOSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MTU1NjM5NiwiZXhwIjoxNzI3ODY5OTk2fQ.2It5EWX_Pvxh2Di3z5zJ9kbIoDcM7ejW96KX534wllg",
       },
     })
       .then((response) => response.json())
-      .then((data) => setCatalog(data.bicycles));
+      .then((data) => setCatalog(data));
   }, []);
 
   return (
