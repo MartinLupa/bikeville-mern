699fc164155626952745540a08ffedd879ebb27d
MartinLupa <lupa_martin@hotmail.com>
2022-03-03 20:23:52 +0100

    shoppingCart limitation bugs solved, functioning

--- client/src/components/AddCartIcon/AddCartIcon.js
+++ client/src/components/AddCartIcon/AddCartIcon.js
@@ -1,20 +1,30 @@
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import { useDispatch, useSelector } from "react-redux";
+import { show } from "../../redux/actions/popup";
 import { addProduct } from "../../redux/reducers/shoppingCartReducer";
 import "./AddCartIcon.css";
 
 export const AddCartIcon = ({ id }) => {
   const dispatch = useDispatch();
   const catalog = useSelector((state) => state.catalog);
+  const shoppingCart = useSelector((state) => state.shoppingCart);
 
   const handleAddClick = () => {
     const addedProduct = catalog.filter((product) => product.product_id === id);
 
-    dispatch(
-      addProduct({
-        ...addedProduct,
-      })
-    );
+    const shop = shoppingCart.products.map((prod) => {
+      return prod[0].product_id;
+    });
+
+    if (shop.includes(addedProduct[0].product_id)) {
+      dispatch(show("That product is already in your shopping cart."));
+    } else {
+      dispatch(
+        addProduct({
+          ...addedProduct,
+        })
+      );
+    }
   };
 
   return (
